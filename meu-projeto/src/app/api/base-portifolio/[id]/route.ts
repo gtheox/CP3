7332be7802f0186011543ad29281bf48fe461be0
gtheox <gteodoro197@gmail.com>
2024-10-27 21:29:41 -0300

    tentativa de corrigir erro ao dar npm run build

--- meu-projeto/src/app/api/base-portifolio/[id]/route.ts
+++ meu-projeto/src/app/api/base-portifolio/[id]/route.ts
@@ -6,57 +6,72 @@ import client from "../../../../../lib/appwrite_client";
 const database = new Databases(client);
 
 // GET para recuperar um trabalho pelo ID
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    try {
-        const response = await database.getDocument(
-            process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
-            process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
-            params.id
-        );
-        return NextResponse.json(response);
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Falha na obtenção da informação: " + error },
-            { status: 500 }
-        );
-    }
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+  
+  try {
+    const response = await database.getDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
+      id
+    );
+    return NextResponse.json(response);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Falha na obtenção da informação: " + (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
 
 // PUT para atualizar um trabalho pelo ID
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-    try {
-        const trabalho = await request.json();
-        await database.updateDocument(
-            process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
-            process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
-            params.id,
-            trabalho
-        );
+export async function PUT(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+  
+  try {
+    const trabalho = await request.json();
+    await database.updateDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
+      id,
+      trabalho
+    );
 
-        return NextResponse.json({ msg: "Trabalho alterado com sucesso!" });
-    } catch (error) {
-        console.error("Falha na atualização do trabalho: ", error);
-        return NextResponse.json(
-            { error: "Falha na atualização do trabalho: " + error },
-            { status: 500 }
-        );
-    }
+    return NextResponse.json({ msg: "Trabalho alterado com sucesso!" });
+  } catch (error) {
+    console.error("Falha na atualização do trabalho: ", error);
+    return NextResponse.json(
+      { error: "Falha na atualização do trabalho: " + (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
 
 // DELETE para excluir um trabalho pelo ID
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-    try {
-        await database.deleteDocument(
-            process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
-            process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
-            params.id
-        );
-        return NextResponse.json({ status: 204 });
-    } catch (error) {
-        console.error("Falha na exclusão do trabalho: ", error);
-        return NextResponse.json(
-            { error: "Falha na exclusão do trabalho: " + error },
-            { status: 500 }
-        );
-    }
+export async function DELETE(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+  
+  try {
+    await database.deleteDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
+      id
+    );
+    return NextResponse.json({ status: 204 });
+  } catch (error) {
+    console.error("Falha na exclusão do trabalho: ", error);
+    return NextResponse.json(
+      { error: "Falha na exclusão do trabalho: " + (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
