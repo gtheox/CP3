90c710781cd161a4d723b2a7273a37381bb9d87e
gtheox <gteodoro197@gmail.com>
2024-10-27 22:23:08 -0300

    oitava tentativa de dar npm run build

--- meu-projeto/src/app/api/base-portifolio/[id]/route.ts
+++ meu-projeto/src/app/api/base-portifolio/[id]/route.ts
@@ -6,44 +6,68 @@ import client from "../../../../../lib/appwrite_client";
 const database = new Databases(client);
 
 // GET para recuperar um trabalho pelo ID
-export async function GET(request:Request,{params}:{params:{id:string}}) {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
+    const response = await database.getDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
+      params.id
+    );
 
-          const response = await database.getDocument(process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string, process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string, params.id);
-      
-      return NextResponse.json(response);
-
+    return NextResponse.json(response);
   } catch (error) {
-      return NextResponse.json({error: "Falha na obtenção da informação."+ error}, {status:500})
+    console.error("Falha na obtenção da informação: ", error);
+    return NextResponse.json(
+      { error: "Falha na obtenção da informação." + String(error) },
+      { status: 500 }
+    );
   }
 }
 
 // PUT para atualizar um trabalho pelo ID
-export async function PUT(request:Request,{params}:{params:{id:string}}) {
-    
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-      const produto = await request.json();
-      await database.updateDocument (process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string, process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string, params.id, produto);
+    const produto = await request.json();
+    await database.updateDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
+      params.id,
+      produto
+    );
 
-      return NextResponse.json({msg:"Trabalho alterado com sucesso!"});
-
+    return NextResponse.json({ msg: "Trabalho alterado com sucesso!" });
   } catch (error) {
-      console.error("Falha na atualização do trabalho: ", error);
-      return NextResponse.json({error:"Falha na atualização do trabalho: "+error});
+    console.error("Falha na atualização do trabalho: ", error);
+    return NextResponse.json(
+      { error: "Falha na atualização do trabalho: " + String(error) },
+      { status: 500 }
+    );
   }
-
 }
 
 // DELETE para excluir um trabalho pelo ID
-export async function DELETE(request:Request,{params}:{params:{id:string}}) {
-    
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-      await database.deleteDocument (process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string, process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string, params.id);
-      return NextResponse.json({status:204});
-
+    await database.deleteDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
+      params.id
+    );
+    return NextResponse.json({ status: 204 });
   } catch (error) {
-      console.error("Falha na exclusão do trabalho: ", error);
-      return NextResponse.json({error:"Falha na exclusão do trabalho : "+error},{status:500});
+    console.error("Falha na exclusão do trabalho: ", error);
+    return NextResponse.json(
+      { error: "Falha na exclusão do trabalho: " + String(error) },
+      { status: 500 }
+    );
   }
-
 }
