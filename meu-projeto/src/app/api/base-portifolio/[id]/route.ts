644957dee7c76d24ad1ca24c83d293a5d239f355
gtheox <gteodoro197@gmail.com>
2024-10-27 22:13:49 -0300

    setima tentativa de dar npm run build

--- meu-projeto/src/app/api/base-portifolio/[id]/route.ts
+++ meu-projeto/src/app/api/base-portifolio/[id]/route.ts
@@ -5,9 +5,6 @@ import client from "../../../../../lib/appwrite_client";
 // Criar um objeto DATABASE passando o arquivo de configuração da plataforma.
 const database = new Databases(client);
 
-// Definição de tipo para os parâmetros da rota
-type Params = { params: { id: string } };
-
 // GET para recuperar um trabalho pelo ID
 export async function GET(request:Request,{params}:{params:{id:string}}) {
   try {
@@ -22,50 +19,31 @@ export async function GET(request:Request,{params}:{params:{id:string}}) {
 }
 
 // PUT para atualizar um trabalho pelo ID
-export async function PUT(
-  request: Request,
-  { params }: Params
-) {
-  const { id } = params;
-
+export async function PUT(request:Request,{params}:{params:{id:string}}) {
+    
   try {
-    const trabalho = await request.json();
-    await database.updateDocument(
-      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
-      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
-      id,
-      trabalho
-    );
+      const produto = await request.json();
+      await database.updateDocument (process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string, process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string, params.id, produto);
 
-    return NextResponse.json({ msg: "Trabalho alterado com sucesso!" });
+      return NextResponse.json({msg:"Trabalho alterado com sucesso!"});
+
   } catch (error) {
-    console.error("Falha na atualização do trabalho: ", error);
-    return NextResponse.json(
-      { error: "Falha na atualização do trabalho: " + (error as Error).message },
-      { status: 500 }
-    );
+      console.error("Falha na atualização do trabalho: ", error);
+      return NextResponse.json({error:"Falha na atualização do trabalho: "+error});
   }
+
 }
 
 // DELETE para excluir um trabalho pelo ID
-export async function DELETE(
-  request: Request,
-  { params }: Params
-) {
-  const { id } = params;
-
+export async function DELETE(request:Request,{params}:{params:{id:string}}) {
+    
   try {
-    await database.deleteDocument(
-      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
-      process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string,
-      id
-    );
-    return NextResponse.json({ status: 204 });
+      await database.deleteDocument (process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string, process.env.NEXT_PUBLIC_APPWRITE_COLLECTIONS_ID as string, params.id);
+      return NextResponse.json({status:204});
+
   } catch (error) {
-    console.error("Falha na exclusão do trabalho: ", error);
-    return NextResponse.json(
-      { error: "Falha na exclusão do trabalho: " + (error as Error).message },
-      { status: 500 }
-    );
+      console.error("Falha na exclusão do trabalho: ", error);
+      return NextResponse.json({error:"Falha na exclusão do trabalho : "+error},{status:500});
   }
+
 }
